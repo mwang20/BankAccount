c0a996e39958049308e41ca762404f79aeda6454
mwang20 <mwang20@stuy.edu>
2020-10-29 00:56:14 -0400

    I tested the deposit method with 1000, 0, and -0. They printed out what I expected

--- Tester.java
+++ Tester.java
@@ -5,5 +5,14 @@ public class Tester{
     System.out.println();
     System.out.println(testBank.getAccountID()); //Should print out 1200
     testBank.setPassword("newpassword"); //Should change password from "password" to "newpassword"
+    System.out.println(testBank.deposit(1000)); //Should return false and add 1000 to the Balance
+    System.out.println(testBank.getBalance());
+    System.out.println();
+    System.out.println(testBank.deposit(0)); //Should return true and add 0 to the Balance
+    System.out.println(testBank.getBalance()); //Should return 1000.00
+    System.out.println();
+    System.out.println(testBank.deposit(-100)); //Should return false and add nothing to the balance
+    System.out.println(testBank.getBalance()); //Should return 1000.0
+    System.out.println();
   }
 }
