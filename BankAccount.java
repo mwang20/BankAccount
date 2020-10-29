66273b68c4b7ae0be7e5420dbd049ec23aa57806
mwang20 <mwang20@stuy.edu>
2020-10-28 23:04:28 -0400

    I made the instance variables and the BankAccount constructor

---
+++ BankAccount.java
@@ -0,0 +1,10 @@
+public class BankAccount{
+  private double balance;
+  private int accountID;
+  private String password;
+  public BankAccount(int accountID, String password){
+    accountID = accountID;
+    password = password;
+    balance = 0;
+  }
+}
