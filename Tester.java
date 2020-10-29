89212dde5f03caf610f3256545761cc8a208073d
mwang20 <mwang20@stuy.edu>
2020-10-29 00:28:52 -0400

    I tested the setPassword method. It should change the password to newpassword

--- Tester.java
+++ Tester.java
@@ -1,8 +1,9 @@
 public class Tester{
   public static void main(String[] args){
-    BankAccount testBank = new BankAccount(2100, "password");
-    System.out.println(testBank.getBalance());
+    BankAccount testBank = new BankAccount(1200, "password");
+    System.out.println(testBank.getBalance()); //Should print out 0.0
     System.out.println();
-    System.out.println(testBank.getAccountID());
+    System.out.println(testBank.getAccountID()); //Should print out 1200
+    testBank.setPassword("newpassword"); //Should change password from "password" to "newpassword"
   }
 }
