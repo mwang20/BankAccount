fa4cbe19ee24652517b7f76e31a6bc159366c809
mwang20 <mwang20@stuy.edu>
2020-10-29 01:13:21 -0400

    I tested the withdraw method with 1000, 0, and -100 and returned the expected values.

--- Tester.java
+++ Tester.java
@@ -5,14 +5,23 @@ public class Tester{
     System.out.println();
     System.out.println(testBank.getAccountID()); //Should print out 1200
     testBank.setPassword("newpassword"); //Should change password from "password" to "newpassword"
-    System.out.println(testBank.deposit(1000)); //Should return false and add 1000 to the Balance
-    System.out.println(testBank.getBalance());
+    System.out.println(testBank.deposit(1000)); //Should return true and add 1000 to the Balance
+    System.out.println(testBank.getBalance()); //Should return 1000.0
     System.out.println();
     System.out.println(testBank.deposit(0)); //Should return true and add 0 to the Balance
     System.out.println(testBank.getBalance()); //Should return 1000.00
     System.out.println();
     System.out.println(testBank.deposit(-100)); //Should return false and add nothing to the balance
     System.out.println(testBank.getBalance()); //Should return 1000.0
+    System.out.println();
+    System.out.println(testBank.withdraw(1000)); //Should return true and subtract 1000 from the Balance
+    System.out.println(testBank.getBalance()); //Should return 0.0
+    System.out.println();
+    System.out.println(testBank.withdraw(0)); //Should return true and subtract 0 from the Balance
+    System.out.println(testBank.getBalance()); //Should return 0.0
+    System.out.println();
+    System.out.println(testBank.withdraw(-100)); //Should return false and subtract nothing from the balance
+    System.out.println(testBank.getBalance()); //Should return 0.0
     System.out.println();
   }
 }
