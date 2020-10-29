f6ab4b34781ac082968fb5eff1d06d338117950b
mwang20 <mwang20@stuy.edu>
2020-10-28 23:50:34 -0400

    I thought the get method was fine but the getAccountID method isn't returning 2100, it is returning 0 for some reason. I have to fix the code and find out whats wrong.

--- Tester.java
+++ Tester.java
@@ -1,5 +1,8 @@
 public class Tester{
   public static void main(String[] args){
     BankAccount testBank = new BankAccount(2100, "password");
+    System.out.println(testBank.getBalance());
+    System.out.println();
+    System.out.println(testBank.getAccountID());
   }
 }
