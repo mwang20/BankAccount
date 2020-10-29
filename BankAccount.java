e9fc0b13cb08ca72d88a22241cf2e2edc843afc6
mwang20 <mwang20@stuy.edu>
2020-10-29 01:11:14 -0400

    Created the withdraw method. Similar to the deposit method except it subtracts from balance

--- BankAccount.java
+++ BankAccount.java
@@ -23,4 +23,11 @@ public class BankAccount{
     }
     return false;
   }
+  public boolean withdraw(double amount){
+    if (amount >= 0 && amount <= balance){
+      balance -= amount;
+      return true;
+    }
+    return false;
+  }
 }
