7ae76003c7051f60c44637cf7431093c84caaa20
mwang20 <mwang20@stuy.edu>
2020-11-01 21:18:52 -0500

    created authenticate method using this, so if the input is equivalent to the password string, it will return true, else it will return false.

--- Tester.java
+++ Tester.java
@@ -5,6 +5,7 @@ public class Tester{
     System.out.println();
     System.out.println(testBank.getAccountID()); //Should print out 1200
     testBank.setPassword("newpassword"); //Should change password from "password" to "newpassword"
+    System.out.println();
     System.out.println(testBank.deposit(1000)); //Should return true and add 1000 to the Balance
     System.out.println(testBank.getBalance()); //Should return 1000.0
     System.out.println();
