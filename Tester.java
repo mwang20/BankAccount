93ae65504de83f54560711b6c29c6af0c0e42c20
mwang20 <mwang20@stuy.edu>
2020-11-01 23:17:36 -0500

    I tested the transferTo method by transferring 50 dollars from BankAccount to other and used the getBalance method to get the balances of BankAccount and other and it printed out 0 and 50 as expected.

--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester{
   public static void main(String[] args){
     BankAccount testBank = new BankAccount(1200, "password");
+    BankAccount other = new BankAccount(1300, "otherpass");
     System.out.println(testBank.getBalance()); //Should print out 0.0
     System.out.println();
     System.out.println(testBank.getAccountID()); //Should print out 1200
@@ -10,24 +11,30 @@ public class Tester{
     System.out.println(testBank.getBalance()); //Should return 1000.0
     System.out.println();
     System.out.println(testBank.deposit(0)); //Should return true and add 0 to the Balance
-    System.out.println(testBank.getBalance()); //Should return 1000.00
+    System.out.println(testBank.getBalance()); //Should return 1000.0
     System.out.println();
     System.out.println(testBank.deposit(-100)); //Should return false and add nothing to the balance
     System.out.println(testBank.getBalance()); //Should return 1000.0
     System.out.println();
-    System.out.println(testBank.withdraw(1000)); //Should return true and subtract 1000 from the Balance
-    System.out.println(testBank.getBalance()); //Should return 0.0
+    System.out.println(testBank.withdraw(950)); //Should return true and subtract 1000 from the Balance
+    System.out.println(testBank.getBalance()); //Should return 50.0
     System.out.println();
     System.out.println(testBank.withdraw(0)); //Should return true and subtract 0 from the Balance
-    System.out.println(testBank.getBalance()); //Should return 0.0
+    System.out.println(testBank.getBalance()); //Should return 50.0
     System.out.println();
     System.out.println(testBank.withdraw(-100)); //Should return false and subtract nothing from the balance
-    System.out.println(testBank.getBalance()); //Should return 0.0
+    System.out.println(testBank.getBalance()); //Should return 50.0
     System.out.println();
     System.out.println(testBank.toString()); //Should return 1200\t0.0
     System.out.println();
-    System.out.println(testBank.authenticate("newpassword")); //Should return true;
+    System.out.println(testBank.authenticate("newpassword")); //Should return true
     System.out.println();
     System.out.println(testBank.authenticate("randompassword")); //Should return false
+    System.out.println();
+    System.out.println(testBank.transferTo(other, 50, "newpassword")); //Should return true
+    System.out.println();
+    System.out.println(testBank.getBalance()); //Should return 0.0
+    System.out.println();
+    System.out.println(other.getBalance()); //Should return 50.0
   }
 }
